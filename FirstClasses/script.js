34c0c9cb5ea7da1e970780c848e7a725c552fe1b
Evan Thacker <thackerevan@gmail.com>
2019-07-25 23:07:14 -0400

    updated script

--- FirstClasses/script.js
+++ FirstClasses/script.js
@@ -1,2 +1,15 @@
-// JavaScript Document
-console.log('Hello World');
+var firstName = 'John';
+
+console.log(firstName);
+
+var lastName = 'Smith';
+
+console.log(lastName);
+
+var age = 28;
+
+console.log(age);
+
+var fullAge = true;
+
+console.log(fullAge);
