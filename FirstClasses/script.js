def5c4cdbb3c442811b7ee4b4d8b85e6c1ddaacd
Evan Thacker <thackerevan@gmail.com>
2019-07-27 02:34:20 -0400

    file updates

--- FirstClasses/script.js
+++ FirstClasses/script.js
@@ -18,6 +18,8 @@ console.log(age);
 
 var fullAge = true;
 
+
+
 //Job Declaried
 
 var job;
@@ -26,10 +28,41 @@ job = 'Teacher';
 
 console.log(job);
 
+
+
+//Variable naming rules
+
+var _3years = 3;
+var johnMark = 'John and Mark';
+var $if = 23;
+
 */
 
 
 
+/****Reviewing Variable Mutation and Type Coercion****/
 
-//Reviewing Variable Mutation and Type Coercion
+var firstName = 'John';
+var age = 28;
 
+
+
+/// console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = 'false';
+
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married?' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+
+
+var lastName = prompt('What is his last Name?')
+console.log(firstName + ' ' + lastName);
