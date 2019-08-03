dc4e851b192d2fef68c1a3dc907f4ce63abafb19
Evan Thacker <thackerevan@gmail.com>
2019-08-03 02:12:04 -0400

    Terminary Opperators

--- secondClasses/sectionTwo.js
+++ secondClasses/sectionTwo.js
@@ -2,6 +2,8 @@
 
 /* If / else statements */
 
+/*
+
 var firstName = 'John';
 var civilStatus = 'single';
 
@@ -17,5 +19,54 @@ if (isMarried) {
 } else {
 	console.log(firstName + ' will hopefully marry soon!');
 }
+
+
+*/
+
+/**** Boolean logic */
+
+/*
+
+var firstName = 'John';
+
+var age = prompt("What is John's age?");
+
+if (age < 13) {
+	console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20){ //Between ages 13 and 20
+console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30){
+			   console.log(firstName + ' is a young man.');
+} else {
+	console.log(firstName + ' is a man.');
+}
+
+*/
+
+
+/******* The Ternary Operator and Switch Statements */
+
+var firstName = prompt('What is your first name?');
+
+var age = prompt('How old is ' + firstName + '?');
+
+age >= 21 ? console.log(firstName + ' drinks beer.')
+: console.log(firstName + ' drinks juice.');
+
+var drink = age >= 21 ? 'beer' : 'juice';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
