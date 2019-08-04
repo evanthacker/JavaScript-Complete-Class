420273ffa2509348c8f972075834a37c35316693
Evan Thacker <thackerevan@gmail.com>
2019-08-04 00:39:31 -0400

    lesson 16 and part of 17

--- secondClasses/sectionTwo.js
+++ secondClasses/sectionTwo.js
@@ -46,24 +46,86 @@ console.log(firstName + ' is a teenager.');
 
 /******* The Ternary Operator and Switch Statements */
 
+/*
+
+
 var firstName = prompt('What is your first name?');
 
 var age = prompt('How old is ' + firstName + '?');
 
-age >= 21 ? console.log(firstName + ' drinks beer.')
-: console.log(firstName + ' drinks juice.');
+/*Ternary operator */
+
+/*
+
+age >= 21 ? /* if statement */ /*
+console.log(firstName + ' drinks beer.')
+: /* else statement */ /* console.log(firstName + ' drinks juice.');
+
+/*Ternary assigned to varable */ 
+/*
 
 var drink = age >= 21 ? 'beer' : 'juice';
 
+console.log(drink);
+
+
+/*If else for Comparision - same*/
+
+/*
+
+if (age >= 18) {
+	var drink = 'beer';
+} else {
+	var drink = 'juice';
+}
+
+***/
+
+/**** switch statements */
+/*
+
+var job = prompt('What is your job?');
+
+switch (job){
+	case 'teacher':
+	case 'professor':
+			console.log(firstName + ' teaches kids how to code.');
+			break;
+	case 'driver':
+			console.log(firstName + ' drives a lift in Tampa.');
+			break;
+	case 'designer':
+			console.log(firstName + ' designs UI/UX for major web company.');
+			break;
+	default:
+		console.log(firstName + ' works somewhere else.');
+}
+
+
+switch(true){
+	case age < 13:
+		console.log(firstName + ' is a boy.');
+		break;
+	case age >= 13 && age < 20:
+		console.log(firstName + ' is a teenager.');
+		break;
+	case age >= 20 && age > 30:
+		console.log(firstName + ' is a young man.');
+		break;
+	default:
+		console.log(firstName + ' is a man.');
+}
+
+
+*/
 
 
 
+/* Truthy and Falsy values and equality operators */
 
+// falsy values: undefined, null, 0, '', NaN
 
-
-
-
-
+// truthy vales: NOT Falsy 
 
 
 
