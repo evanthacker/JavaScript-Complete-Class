1459193c6e0bfc23a7efdfe4361357a31824689a
Evan Thacker <thackerevan@gmail.com>
2019-08-05 11:17:38 -0400

    second Project

--- secondProject/script.js
+++ secondProject/script.js
@@ -13,7 +13,7 @@ John and Mike both play Basketball on different teams. In the latest 3 games, Jo
 
 3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score).
 
-4. EXTRA: Mary also plays basketball, and her team scored 97, 134, and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the the decision. If you can't solve this one, just watch the solution, it's no problem. :D
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134, and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem. :D
 
 
 5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
@@ -21,4 +21,80 @@ John and Mike both play Basketball on different teams. In the latest 3 games, Jo
 GOOD LUCK :D
 */
 
-console.log("Hi Evan");
+var TeamOne = "John";
+
+var TeamTwo = "Mike";
+
+var TeamThree = "Mary";
+
+
+
+var TeamOneFirst = 89;
+
+var TeamOnesecond = 120;
+	
+var TeamOneThird = 103;
+
+var TeamOnesum = TeamOneFirst + TeamOnesecond + TeamOneThird;
+
+var TeamOneAverage = TeamOnesum / 3;
+
+console.log(TeamOne + " has an average score of:" + TeamOneAverage);
+
+
+var TeamTwoFirst = 116;
+
+var TeamTwosecond = 94;
+
+var TeamTwoThird = 123;
+
+
+var TeamTwosum = TeamTwoFirst + TeamTwosecond + TeamTwoThird;
+
+var TeamTwoAverage = TeamTwosum / 3; 
+
+
+console.log(TeamTwo + " has an average score of:" + TeamTwoAverage);
+
+
+
+var TeamThreeFirst = 97;
+
+var TeamThreesecond = 134;
+
+var TeamThreeThird = 105;
+
+var TeamThreesum = TeamThreeFirst + TeamThreesecond + TeamThreeThird;
+
+
+var TeamThreeAverage = TeamThreesum / 3; 
+
+
+console.log(TeamThree + " has an average score of:" + TeamThreeAverage);
+
+
+if (TeamOneAverage > TeamTwoAverage && TeamOneAverage > TeamThreeAverage) {
+	console.log(TeamOne + " Wins!");
+} else if (TeamTwoAverage > TeamOneAverage && TeamTwoAverage > TeamThreeAverage){
+console.log(TeamTwo + " Wins!");
+} else if (TeamThreeAverage > TeamOneAverage && TeamThreeAverage > TeamTwoAverage){
+console.log(TeamThree + " Wins!");
+}	else {
+	console.log("It's a draw.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
