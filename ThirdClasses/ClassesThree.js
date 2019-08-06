2633211ad0b4ab96b07e610a0f89e3aa95ff6b40
Evan Thacker <thackerevan@gmail.com>
2019-08-05 22:48:47 -0400

    Third Classes

---
+++ ThirdClasses/ClassesThree.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+
+/***** Functions **/
+
+
+/*
+function calculateAge(birthYear){
+	return 2019 - birthYear;
+	
+}
+
+
+var ageJohn = calculateAge(1990);
+
+var ageMike = calculateAge(1958);
+
+var ageJane = calculateAge(1980);
+
+
+console.log(ageJohn, ageMike, ageJane);
+
+
+function yearsUntilRetirement(year, firstName){
+	var age = calculateAge(year);
+	var retirement = 65 - age;
+	
+	if(retirement > 0){
+		console.log(firstName + " retires in " + retirement + " years.");
+	} else {
+		console.log(firstName + " is already retired.")
+	}
+
+}
+
+yearsUntilRetirement(1990, 'John');
+
+yearsUntilRetirement(1969, 'Mike');
+
+yearsUntilRetirement(1954, 'Ann');
+
+*/
+
+
+/**** Function Statements and Expressions ***/
+
